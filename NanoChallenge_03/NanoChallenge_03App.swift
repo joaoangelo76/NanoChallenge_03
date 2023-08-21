d0c51882a9c227423840977c22800c8db8bb33bb
João <joaoasn0409@gmail.com>
2023-08-21 13:54:10 -0300

    Initial Commit

---
+++ NanoChallenge_03/NanoChallenge_03App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge_03App.swift
+//  NanoChallenge_03
+//
+//  Created by João Ângelo on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
