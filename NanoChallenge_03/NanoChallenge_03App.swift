2f2dd521becb1011dc0aab87469366f0a7a85b5a
Enrique-mac <carvalhoenrique33@gmail.com>
2023-08-21 17:59:53 -0300

    added core data base support

--- NanoChallenge_03/NanoChallenge_03App.swift
+++ NanoChallenge_03/NanoChallenge_03App.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct NanoChallenge_03App: App {
+    @StateObject private var coreDataControler = CoreDataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, coreDataControler.container.viewContext)
         }
     }
 }
