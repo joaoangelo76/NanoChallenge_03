c6c07eff5c50673bca88a1beb56afdc5c8097704
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-21 17:22:57 -0300

    Added Errors

--- NanoChallenge_03/API/Models/NetworkingErrors.swift
+++ NanoChallenge_03/API/Models/NetworkingErrors.swift
@@ -8,3 +8,29 @@
 import Foundation
 
 
+enum GHError: Error {
+    
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
+
+
+/*
+ 
+ .task {
+     do {
+ //colocar user no @State da Content 
+       user = try await getGames()
+     } catch GHError.invalidURL {
+         print("innvalid URL")
+     } catch GHError.invalidResponse {
+         print("invalid response")
+     } catch GHError.invalidData {
+         print("invalid data")
+     } catch {
+         print("unexpected error")
+     }
+ }
+
+ */
