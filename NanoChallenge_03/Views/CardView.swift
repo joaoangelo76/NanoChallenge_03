8a67bea36a90d3dcd29d5a54d8c9129e83f7267b
MateusMPires <mateusmp.df@gmail.com>
2023-08-21 22:19:44 -0300

    Added a CardView for all games

---
+++ NanoChallenge_03/Views/CardView.swift
@@ -0,0 +1,23 @@
+//
+//  CardView.swift
+//  NanoChallenge_03
+//
+//  Created by Mateus Martins Pires on 21/08/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @State var gameArray: [Game] = []
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
