b48e7bea471e7dba7949dd67232b8542120f3228
RoxasTA <victorbr64g@gmail.com>
2023-08-23 13:49:58 -0300

    Save favoritos

--- NanoChallenge_03/Views/DetailView.swift
+++ NanoChallenge_03/Views/DetailView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.managedObjectContext) var mangedObjContext
+    @Environment(\.dismiss) var dismiss
+    @State private var favorito: Bool = false
+    @State private var nomejogo: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form{
+            Section{
+                //TextField("Nome do jogo", text: $nomejogo)
+                Toggle(isOn: $favorito, label: {
+                    Text("Favorito")
+                        .foregroundColor(.accentColor)
+                })
+            }
+        }
+        /*if favorito == true{
+            CoreDataController().addGame(title: <#T##String#>, thumbnail: <#T##String#>, gameDescription: <#T##String#>, genre: <#T##String#>, publisher: <#T##String#>, releaseDate: <#T##String#>, context: <#T##NSManagedObjectContext#>)
+        }*/
     }
 }
-
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView()
