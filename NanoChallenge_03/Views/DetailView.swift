af13feb529e270b0efff279060b12d7fb5e17406
Guilherme Nunes Lobo <loboguilherme2003@gmail.com>
2023-08-25 15:14:02 -0300

    Update DetailView.swift
    Favorite button modified to receive toggle animation

--- NanoChallenge_03/Views/DetailView.swift
+++ NanoChallenge_03/Views/DetailView.swift
@@ -37,10 +37,40 @@ struct DetailView: View {
             Text(detailedGame?.releaseDate ?? "")
                 .padding()
             Button {
-                addGame()
+                // Verifica se há um ID de jogo disponível
+                if let gameID = detailedGame?.id {
+                    // Verifica se o jogo já está na lista de favoritos pelo seu ID
+                    if let existingIndex = coreDataController.savedGames.firstIndex(where: { $0.id == gameID }) {
+                        // Se o jogo já está na lista de favoritos, remove-o
+                        coreDataController.deleteUser(indexSet: IndexSet([existingIndex]))
+                    } else {
+                        // Se o jogo não está na lista de favoritos, adiciona-o
+                        coreDataController.addGame(
+                            id: gameID,
+                            title: detailedGame?.title ?? "",
+                            thumbnail: detailedGame?.thumbnail ?? "",
+                            gameDescription: detailedGame?.shortDescription ?? "",
+                            genre: detailedGame?.genre ?? "",
+                            publisher: detailedGame?.publisher ?? "",
+                            releaseDate: detailedGame?.releaseDate ?? ""
+                        )
+                    }
+                }
             } label: {
-                Image(systemName: "star")
+                // Verifica se há um ID de jogo disponível para alterar o ícone do botão
+                if let gameID = detailedGame?.id {
+                    // Altera o ícone do botão para preenchido ("star.fill") se o jogo estiver na lista de favoritos, caso contrário, usa o ícone vazio ("star")
+                    Image(systemName: coreDataController.savedGames.contains { $0.id == gameID } ? "star.fill" : "star")
+                } else {
+                    // Usa o ícone vazio ("star") como padrão se não houver ID de jogo
+                    Image(systemName: "star")
+                }
             }
+
+
+
+
+
         }
         .foregroundColor(.black)
     }
